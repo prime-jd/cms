dea708d69daa343ee529b11c41070feb0d9c3cb6
prime_jd <ravishankartripathi2002@gmail.com>
2024-04-21 15:38:39 +0530

    app

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from 'express';  
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,         // accepting responses from all origins
+    credentials : true
+}));
+
+app.use(express.json({limit : "16kb"}));       // if data recieved from json
+app.use(express.urlencoded({extended : true, limit : "16kb"}));     // if data recieved from url
+app.use(express.static("public"));             // if  i want to save any file folder or image in public folder  
+app.use(cookieParser());
+
+export {app}
