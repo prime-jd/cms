cc412e0d6ebd175788fdf4389eabf0bdc4fa6557
prime_jd <ravishankartripathi2002@gmail.com>
2024-04-22 21:29:03 +0530

    video model

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -12,5 +12,22 @@ const userSchema = mongoose.Schema({
         lowecase : true,
         trim : true,
         index : true
+    },
+    password: {
+        type : String,
+        required : true,
+        unique : true,
+        trim : true,
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+        lowecase : true,
+        trim : true,
+        index : true
     }
+
 })
+
+export const User = mongoose.model("User", userSchema);
