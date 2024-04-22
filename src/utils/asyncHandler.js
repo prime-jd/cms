097c0dbc449e46343f338c86309d422bd306e7f7
prime_jd <ravishankartripathi2002@gmail.com>
2024-04-22 20:28:38 +0530

    async handler utility

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+// every time when we req data from db there is always async used to ease this process we use utility function to avoid all the bulky code
+
+// it takes function as parameter hence higher order function
+
+
+// promise method
+
+const asyncHandler = (requestHandler) => (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error)=>(next(error)));
+}
+
+
+
+
+
+// try catch method
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+        
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message,
+//         })
+//     }
+// }
