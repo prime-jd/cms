34cf85060221ade20a6df23c7b51bd20bdbd9476
prime_jd <ravishankartripathi2002@gmail.com>
2024-04-25 20:45:52 +0530

    usercontroller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,13 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Register User" });
-    res.send("Register User");
+    
+
+    // get user details from frontend
+    const {email,username,password,fullname}= req.body
+    console.log(email)
+
+    // check validation 
     
 });
 
