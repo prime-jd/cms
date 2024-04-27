b6607909d4774fb3b18d21ab91faeb1349cbcf97
prime_jd <ravishankartripathi2002@gmail.com>
2024-04-27 05:47:53 +0530

    authorization middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+import { ApiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.model.js";
+
+export const verifyJwt = asyncHandler(async (req,_,next)=>{
+    try {
+        const token =req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+        console.log(token)
+        if(!token){
+            throw new ApiError(404, "Unauthorized Request")
+        }
+        const decodedToken =jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken?.id).select("-password -refreshToken")
+        
+        if(!user){
+            //frontend ...
+            throw new ApiError(404, "Unauthorized user Request")
+        }
+        req.user = user
+        next()
+        
+    } catch (error) {
+        throw new ApiError(401, error.message ||"Unauthorized Request")
+    }
+});
