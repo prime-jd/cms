0816024967c125accc585dc80b062debdb0872c7
prime_jd <ravishankartripathi2002@gmail.com>
2024-04-27 06:36:49 +0530

    user routers

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser,loginUser,logoutUser } from "../controllers/user.controller.js";
+import { registerUser,loginUser,logoutUser,refreshAccessToken } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
@@ -16,5 +16,6 @@ router.route("/login").post(loginUser)
 
 // secured routes
 router.route("/logout").post(verifyJwt , logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router;
