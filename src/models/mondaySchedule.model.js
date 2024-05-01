cee1b762f53e36213f43f31fac1ca522c028dd70
prime_jd <ravishankartripathi2002@gmail.com>
2024-05-01 08:16:50 +0530

    fd

--- src/models/mondaySchedule.model.js
+++ src/models/mondaySchedule.model.js
@@ -25,51 +25,59 @@ const MondaySchema = new mongoose.Schema({
     mark : {
         type : Boolean,
         required : true
+    },
+    day : {
+        type : String,
+        required : true
     }
     
 });
 
 export const Monday = mongoose.model("Monday", MondaySchema);
 
-const MondayData = [
-    {
-        subject: "Mathematics",
-        subjectCode: "MATH",
-        faculty: "Dr. Rakesh",
-        time: "9:30 AM - 10:20 AM",
-        status :false,
-        mark : false,
-    },
-    {
-        subject: "English",
-        subjectCode: "ENG",
-        faculty: "Dr. Rakesh",
-        time: "10:20 AM - 11:10 AM",
-        status: false,
-        mark  : false
-    },
-    {
-        subject: "Science",
-        subjectCode: "SCI",
-        faculty: "Dr. Rakesh",
-        time: "11:10 AM - 12:50 PM",
-        status: false,
-        mark : false
-    },
-    {
-        subject: "Social Science",
-        subjectCode: "SS",
-        faculty: "Dr. Rakesh",
-        time: "12:50 PM - 1:40 PM",
-        status: false,
-        mark : false
-    },
-    {
-        subject: "Web Technology",
-        subjectCode: "CS",
-        faculty: "Ajeet Kr. Bhartee",
-        time: "2:30 PM - 3:20 PM",
-        status: false,
-        mark : false
-    },
-];
+// const MondayData = [
+//     {
+//         subject: "Mathematics",
+//         subjectCode: "MATH",
+//         faculty: "Dr. Rakesh",
+//         time: "01:30 AM - 02:20 AM",
+//         status :false,
+//         mark : false,
+//     },
+//     {
+//         subject: "English",
+//         subjectCode: "ENG",
+//         faculty: "Dr. Rakesh",
+//         time: "10:20 AM - 11:10 AM",
+//         status: false,
+//         mark  : false
+//     },
+//     {
+//         subject: "Science",
+//         subjectCode: "SCI",
+//         faculty: "Dr. Rakesh",
+//         time: "11:10 AM - 12:50 PM",
+//         status: false,
+//         mark : false
+//     },
+//     {
+//         subject: "Social Science",
+//         subjectCode: "SS",
+//         faculty: "Dr. Rakesh",
+//         time: "12:50 PM - 1:40 PM",
+//         status: false,
+//         mark : false
+//     },
+//     {
+//         subject: "Web Technology",
+//         subjectCode: "CS",
+//         faculty: "Ajeet Kr. Bhartee",
+//         time: "2:30 PM - 3:20 PM",
+//         status: false,
+//         mark : false
+//     },
+// ];
+
+// Monday.insertMany(MondayData)
+//     .then(() => console.log('Documents inserted into "Monday" collection'))
+//     .catch(err => console.error('Error inserting documents:', err));
