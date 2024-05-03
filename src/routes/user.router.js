5ff359b2255fcd471be3aa68d8f7b8eb75b2ae1a
prime_jd <ravishankartripathi2002@gmail.com>
2024-05-03 23:59:15 +0530

    user router

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -18,7 +18,7 @@ router.route("/login").post(loginUser)
 // secured routes
 router.route("/logout").post(verifyJwt , logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
-router.route("/schedule").post(updateTimeTable)
+router.route("/schedule").post(verifyJwt, updateTimeTable)
 router.route("/schedule-in").post(onTimeTable)
 router.route("/schedule-out").post(getTimeTable)
 
