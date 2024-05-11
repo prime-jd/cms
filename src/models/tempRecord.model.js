d5d53edc6fefcdcfa8072b4de38c5593fc367c44
prime_jd <ravishankartripathi2002@gmail.com>
2024-05-11 22:28:43 +0530

    ..

---
+++ src/models/tempRecord.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const recordSchema= new mongoose.Schema({
+
+    date : {
+        type : String,
+        required : true
+    },
+    time : {
+        type : String,
+            
+    },
+    subject : {
+        type : String,
+       
+    },
+    faculty : {
+        type : String,
+       
+    },
+    rollNo : {
+        type : String, 
+        
+    },
+    startTime : {
+        type: String,
+       
+    },
+    proxy : {
+        type : Boolean,
+
+    },
+    className : {
+        type : String,
+        required : true
+    },
+    otp :{
+        type :Number
+    }
+
+})
+
+export const Record = mongoose.model("Record",recordSchema);
