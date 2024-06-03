eff415529731c3a25a550d03cf781f5b4bcf4e73
prime_jd <ravishankartripathi2002@gmail.com>
2024-06-03 15:49:24 +0530

    .

---
+++ api/hello.js
@@ -0,0 +1,19 @@
+// // myFunction.js
+
+// // Import necessary modules
+// import { Router } from 'express';
+
+// // Create a new router
+// const router = Router();
+
+// // Define the route handler function
+// const myFunctionHandler = (req, res) => {
+//   // Logic for your function
+//   res.status(200).json({ message: 'This is my serverless function!' });
+// };
+
+// // Define the route and attach the handler function
+// router.get('/my-function', myFunctionHandler);
+
+// // Export the router
+// export default router;
