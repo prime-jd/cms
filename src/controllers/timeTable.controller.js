e6304172164debf0c4577520dd17385240527e2c
prime_jd <ravishankartripathi2002@gmail.com>
2024-06-04 17:28:39 +0530

    apierror path

--- src/controllers/timeTable.controller.js
+++ src/controllers/timeTable.controller.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Monday } from "../models/mondaySchedule.model.js";
-import { ApiError } from "../utils/apiError.js";
+import { ApiError } from '../utils/ApiError.js';
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 import { Teacher } from "../models/teacher.model.js";
